e2765c88939ceb4662f68239e37e7b25d2aac865
LFedorov <LFedorov@users.noreply.github.com>
2020-08-15 14:34:45 +0000

    deploy: 9a1dcd30988b8281cd0187edb30ca70faa243f39

---
+++ precache-manifest.d36413f4e0ed2652b5aef1d29a54b73b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb839d9e6795a187464ccf990439aea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5909c7a3c576e51c2ca3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b78643a42955d27073ed",
+    "url": "/static/js/2.f60ebbdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f60ebbdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5909c7a3c576e51c2ca3",
+    "url": "/static/js/main.898b907c.chunk.js"
+  },
+  {
+    "revision": "297e78c7b9a452f0c5cf",
+    "url": "/static/js/runtime-main.c2fd0bb8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
